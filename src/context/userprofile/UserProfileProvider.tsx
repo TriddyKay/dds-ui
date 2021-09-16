a30d64ef9065d0881da13ac037af1e06112574d1
TriddyKay <tristankay93@gmail.com>
2021-09-16 22:23:29 +1000

    cache userprofile in localstorage

--- src/context/userprofile/UserProfileProvider.tsx
+++ src/context/userprofile/UserProfileProvider.tsx
@@ -1,9 +1,23 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import {UserProfileContext} from './UserProfileContext'
 import {UserProfile} from "../../domain/UserProfile"
 
 export const UserProfileProvider: React.FC = ({children}) => {
   const [userProfile, setUserProfile] = useState<UserProfile | null>(null)
+
+  useEffect(() => {
+    const localizedUserProfile = localStorage.getItem("userprofile")
+
+    if(localizedUserProfile) {
+      setUserProfile(JSON.parse(localizedUserProfile))
+    }
+  },[])
+
+  useEffect(() => {
+   userProfile
+     ? localStorage.setItem("userprofile", JSON.stringify(userProfile))
+     : localStorage.removeItem("userprofile")
+  }, [userProfile])
 
   const signInUser = (userProfile: UserProfile) => {
     setUserProfile(userProfile)
